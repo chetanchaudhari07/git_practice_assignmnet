4523b62c39df8c9f7d242a2270c4c4e7b2fe8bb7
chetanchaudhari07 <chetancdi077@gmail.com>
2024-03-01 14:40:08 +0530

    incorrect code

---
+++ primenumber.js
@@ -0,0 +1,21 @@
+let number = 17; 
+
+let isPrime = true;
+
+if (number < 2) {
+    isPrime = false;
+} else {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+}
+]
+}
+    }
+}
+
+console.log(number + (isPrime ? " is a prime number." : " is not a prime number."));
